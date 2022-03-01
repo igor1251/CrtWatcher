8890d5c87ab37e3c099d3e52fc3c10905b116690
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-03-01 16:56:50 +0300

    Добавил конфигуратор службы

---
+++ helpful soft/ObserverConfigurator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObserverConfigurator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(connectionPortTextBox.Text) ||
+                string.IsNullOrEmpty(serverIPTextBox.Text))
+            {
+                MessageBox.Show("Fill in all the fields!");
+            }
+            else
+            {
+                var settingsStorage = new SettingsStorage();
+                await settingsStorage.SaveSettingsToFile(new Settings
+                {
+                    MainServerIP = serverIPTextBox.Text,
+                    MainServerPort = int.Parse(connectionPortTextBox.Text),
+                    VerificationFrequency = 10
+                });
+                MessageBox.Show("Saved!");
+            }
+        }
+    }
+}
