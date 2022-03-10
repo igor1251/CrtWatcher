336f16362e09fd20c66b927d090905379c343592
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-03-10 21:21:02 +0300

    Конфигуратор создан

--- helpful soft/Configurator/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ helpful soft/Configurator/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace Configurator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Configurator;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Configurator;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
