a7ae9c41adb64a45c013d34bd0854d476295440c
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-04-05 16:08:53 +0300

    Добавил несколько звпросов

--- libs/X509Observer/MagicStrings/DatabaseQueries/SubjectsRepositoryQueries.cs
+++ libs/X509Observer/MagicStrings/DatabaseQueries/SubjectsRepositoryQueries.cs
@@ -2,13 +2,14 @@
 {
     public static class SubjectsRepositoryQueries
     {
-        public static readonly string GET_SUBJECTS = "",
-                                      GET_SUBJECT_BY_ID = "",
-                                      GET_DIGITAL_FINGERPRINTS = "",
-                                      ADD_SUBJECT = "",
-                                      ADD_DIGITAL_FINGERPRINT = "",
-                                      UPDATE_SUBJECT = "",
-                                      REMOVE_DIGITAL_FINGERPRINT_BY_ID = "",
-                                      REMOVE_SUBJECT_BY_ID = "";
+        public static readonly string GET_SUBJECTS = "SELECT * FROM [Subjects];",
+                                      GET_SUBJECT_BY_ID = "SELECT * FROM [Subjects] WHERE ID=@ID;",
+                                      GET_DIGITAL_FINGERPRINTS = "SELECT * FROM [DigitalFingerprints];",
+                                      ADD_SUBJECT = "INSERT INTO [Subjects] (Name, Phone) VALUES (@Name, @Phone);",
+                                      ADD_DIGITAL_FINGERPRINT = "INSERT INTO [DigitalFingerprints] (SubjectID, Hash, Start, End) VALUES (@SubjectID, @Hash, @Start, @End);",
+                                      UPDATE_SUBJECT = "UPDATE [Subjects] SET Name=@Name, Phone=@Phone WHERE ID=@ID;",
+                                      REMOVE_DIGITAL_FINGERPRINT_BY_ID = "DELETE FROM [DigitalFingerprints] WHERE ID=@ID;",
+                                      REMOVE_SUBJECT_BY_ID = "DELETE FROM [Subjects] WHERE ID=@ID;",
+                                      GET_SUBJECT_ID = "SELECT ID FROM [Subjects] WHERE [Subjects].Name=@Name";
     }
 }
