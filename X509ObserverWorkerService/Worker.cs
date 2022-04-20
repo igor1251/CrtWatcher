84ae85319ce902a3d2ae4da7ab995f9bbda983ee
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-04-20 14:07:01 +0300

    Начал делать службу

--- X509ObserverWorkerService/Worker.cs
+++ X509ObserverWorkerService/Worker.cs
@@ -23,16 +23,25 @@ namespace X509ObserverWorkerService
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
+            _configuration["ApiKey"] = "test-api-key";
+            _logger.LogInformation("Settings stored in appsettings.json:\n" +
+                                   "\tRemoteRegistrationServiceAddress: " + _configuration["RemoteRegistrationServiceAddress"] + "\n" +
+                                   "\tRemoteAuthenticationServiceAddress: " + _configuration["RemoteAuthenticationServiceAddress"] + "\n" +
+                                   "\tRemoteX509VaultStoreService: " + _configuration["RemoteX509VaultStoreService"] + "\n" +
+                                   "\tRemoteServiceLogin: " + _configuration["RemoteServiceLogin"] + "\n" +
+                                   "\tRemoteServicePassword: " + _configuration["RemoteServicePassword"] + "\n" +
+                                   "\tApiKey: " + _configuration["ApiKey"] + "\n" +
+                                   "\tMonitoringInterval: " + _configuration["MonitoringInterval"] + "\n");
             return base.StartAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
+            //    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            //    await Task.Delay(1000, stoppingToken);
+            //}
         }
 
         public override Task StopAsync(CancellationToken cancellationToken)
