bccadd5f16625bd28382a65f658783818d4e9b53
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-04-28 15:31:59 +0300

    Добавил клиента

---
+++ X509ObserverClient-GUI/View/LoginWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using NetworkOperators.Identity.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace X509ObserverClient_GUI.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public UserAuthorizationRequest GetUserAuthorizationRequest()
+        {
+            return new UserAuthorizationRequest() 
+            { 
+                UserName = loginTextBox.Text, 
+                Password = passwordBox.Password 
+            };
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(loginTextBox.Text) &&
+                !string.IsNullOrEmpty(passwordBox.Password))
+            {
+                this.DialogResult = true;
+            }
+            else this.DialogResult = null;
+        }
+    }
+}
---
+++ X509ObserverClient-GUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X509ObserverClient_GUI.ViewModel
+{
+    public class MainViewModel
+    {
+    }
+}
