99b96943993f2538d61aae97320afa43e3a3bf0c
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-05-05 14:34:37 +0300

    Добавил пару контроллеров

---
+++ X509ObserverAdmin/Controllers/SettingsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using X509ObserverAdmin.Models;
+
+namespace X509ObserverAdmin.Controllers
+{
+    [Authorize]
+    public class SettingsController : Controller
+    {
+        private readonly ILogger<SettingsController> _logger;
+
+        public SettingsController(ILogger<SettingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ X509ObserverAdmin/Controllers/SubjectsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using X509ObserverAdmin.Models;
+
+namespace X509ObserverAdmin.Controllers
+{
+    [Authorize]
+    public class SubjectsController : Controller
+    {
+        private readonly ILogger<SubjectsController> _logger;
+
+        public SubjectsController(ILogger<SubjectsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
